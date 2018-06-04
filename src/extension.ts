8bfe1c5a6bcad33b3f15a38cca9926fee72854be
Per Persson <md2perpe@gmail.com>
2018-06-04 23:55:16 +0200

    Simplify code

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,8 @@
 'use strict';
 
-import { Disposable, ExtensionContext} from 'vscode';
+import { ExtensionContext } from 'vscode';
 import MeshPreviewContentProvider from './contentProvider';
 
-let _disposables: Disposable[] = [];
-
 export function activate(context: ExtensionContext) {
-
-    context.subscriptions.push(new Disposable(() => Disposable.from(..._disposables).dispose()));
-    
-    _disposables.push( new MeshPreviewContentProvider(context) );
-
+    context.subscriptions.push(new MeshPreviewContentProvider(context));
 }
