a40696f84a00091d394de80610ebbf840488ab50
Per Persson <md2perpe@gmail.com>
2018-06-04 23:49:53 +0200

    Remove editor provider

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 
 import { Disposable, ExtensionContext} from 'vscode';
 import MeshPreviewContentProvider from './contentProvider';
-import EditorProvider from './editorProvider';
 
 let _disposables: Disposable[] = [];
 
@@ -11,7 +10,5 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(new Disposable(() => Disposable.from(..._disposables).dispose()));
     
     _disposables.push( new MeshPreviewContentProvider(context) );
-
-    // _disposables.push( new EditorProvider(context) );
 
 }
